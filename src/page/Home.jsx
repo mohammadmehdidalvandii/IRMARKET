692a59588eaf1bd07795a7d4992b3816a8c883f4
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-12-31 17:27:12 +0330

    create section homePoster

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,13 @@
 import HomeHeader from "../components/template/home/HomeHeader/HomeHeader";
 import HomeDiscount from "../components/template/home/HomeDiscount/HomeDiscount";
+import HomePoster from "../components/template/home/HomePoster/HomePoster";
 
 function Home() {
   return (
    <>
     <HomeHeader/>
     <HomeDiscount/>
+    <HomePoster/>
     <br /><br /><br /><br />
    </>
   )
