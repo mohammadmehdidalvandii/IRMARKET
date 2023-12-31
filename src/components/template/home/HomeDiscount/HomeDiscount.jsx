2169187c11f85b23f7035f04fa30da503849df96
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-12-31 18:09:38 +0330

    add method slice for products

--- src/components/template/home/HomeDiscount/HomeDiscount.jsx
+++ src/components/template/home/HomeDiscount/HomeDiscount.jsx
@@ -20,7 +20,7 @@ function HomeDiscount() {
                         </div>
                     </div>
                     <div className="row mt-5">
-                        {productsDate.products.map(product=> (
+                        {productsDate.products.slice(0,6).map(product=> (
                         <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12" key={product.id}>
                             <ProductCard
                                {...product}
