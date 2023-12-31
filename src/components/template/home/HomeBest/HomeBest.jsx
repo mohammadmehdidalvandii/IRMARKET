0d6d3d4c69bada1e23e5df12c33eb1cc5906e763
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-12-31 19:19:21 +0330

    add toLocaleString price

--- src/components/template/home/HomeBest/HomeBest.jsx
+++ src/components/template/home/HomeBest/HomeBest.jsx
@@ -25,6 +25,7 @@ function HomeBest() {
             <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12" key={product.id}>
                 <ProductCard
                     {...product}
+                    price={product.price.toLocaleString()}
                 />
             </div>
             ))}
--- src/components/template/home/HomeDiscount/HomeDiscount.jsx
+++ src/components/template/home/HomeDiscount/HomeDiscount.jsx
@@ -24,6 +24,7 @@ function HomeDiscount() {
                         <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12" key={product.id}>
                             <ProductCard
                                {...product}
+                               price={product.price.toLocaleString()}
                             />
                         </div>
                         ))}
