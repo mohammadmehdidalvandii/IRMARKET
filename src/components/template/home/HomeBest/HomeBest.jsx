07fcd5b295dc95e0598ed014f85a739d3379bb7e
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-12-31 18:07:34 +0330

    create section homebest

--- src/components/template/home/HomeBest/HomeBest.jsx
+++ src/components/template/home/HomeBest/HomeBest.jsx
@@ -1,24 +1,38 @@
-import Link from '../../../module/Link/Link'
-import Title from '../../../module/Title/Title'
-import './HomeBest.css'
+import "./HomeBest.css";
+import Title from "../../../module/Title/Title";
+import Link from "../../../module/Link/Link";
+import ProductCard from "../../../module/ProductCard/ProductCard";
+import productContext from "../../../../context/productContext";
+import { useContext } from "react";
 
 function HomeBest() {
+
+    const productData = useContext(productContext)
   return (
     <section className="homeBest">
-        <div className="containers">
-            <div className="contentBox">
-            <div className="row">
-                <div className="col-lg-6">
-                    <Title title='بهترین کالا ها'/>
-                </div>
-                <div className="col-lg-6">
-                    <Link/>
-                </div>
+      <div className="containers">
+        <div className="contentBox">
+          <div className="row">
+            <div className="col-lg-6">
+              <Title title="بهترین کالا ها" />
             </div>
+            <div className="col-lg-6">
+              <Link />
             </div>
+          </div>
+          <div className="row mt-5">
+            {productData.products.slice(6.7).map(product=>(
+            <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12" key={product.id}>
+                <ProductCard
+                    {...product}
+                />
+            </div>
+            ))}
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default HomeBest
+export default HomeBest;
