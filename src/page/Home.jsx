dc372122570c09e96a8376b83d949cc98f7060d6
mohammadmehdi <mohammad333mehdi@gmail.com>
2024-01-01 15:14:13 +0330

    create section posterDiscount for page home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,6 +2,7 @@ import HomeHeader from "../components/template/home/HomeHeader/HomeHeader";
 import HomeDiscount from "../components/template/home/HomeDiscount/HomeDiscount";
 import HomePoster from "../components/template/home/HomePoster/HomePoster";
 import HomeBest from "../components/template/home/HomeBest/HomeBest";
+import PosterDiscount from "../components/template/home/PosterDiscount/PosterDiscount";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
     <HomeDiscount/>
     <HomePoster/>
     <HomeBest/>
+    <PosterDiscount/> 
     <br /><br /><br /><br />
    </>
   )
