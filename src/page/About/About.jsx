475d7002a3a17c7c1ba46f84446f1b0bbb8c54a6
mohammadmehdi <mohammad333mehdi@gmail.com>
2024-01-02 15:52:25 +0330

    create section aboutUs

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -1,9 +1,11 @@
 import AboutHeader from "../../components/template/about/AboutHeader/AboutHeader"
+import AboutUs from "../../components/template/about/AboutUs/AboutUs"
 
 function About() {
   return (
    <>
    <AboutHeader/>
+   <AboutUs/>
    </>
   )
 }
