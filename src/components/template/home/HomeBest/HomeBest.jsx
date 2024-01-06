4d1823c753a122b112e8415920edf047fe1748c9
mohammadmehdi <mohammad333mehdi@gmail.com>
2024-01-06 14:52:47 +0330

    fix productBest for homePage

--- src/components/template/home/HomeBest/HomeBest.jsx
+++ src/components/template/home/HomeBest/HomeBest.jsx
@@ -21,7 +21,7 @@ function HomeBest() {
             </div>
           </div>
           <div className="row mt-5">
-            {productData.products.slice(6.7).map(product=>(
+            {productData.products.slice(6 , 12).map(product=>(
             <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12" key={product.id}>
                 <ProductCard
                     {...product}
